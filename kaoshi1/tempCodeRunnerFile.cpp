dd4fcb8bda1ae0763402115cabd22ff489309495
lenovo <lenovo@LAPTOP-EJ9ASDJ8>
2020-07-21 22:17:38 +0800

    mo

--- kaoshi1/tempCodeRunnerFile.cpp
+++ kaoshi1/tempCodeRunnerFile.cpp
@@ -1,4 +1,19 @@
-#include <iostream>
-#include<string>
-#include<cstring>
-#include<cmath>
+#include<iostream>
+using namespace std;
+int main(){
+    char a[100];
+    int ge = 0;
+    while(cin >> a[ge]){
+        ge++;
+    }
+    ge++;
+    if(a[0] == '-'){
+        for(int i = 1;i<ge;i++){
+            cout << a[i];
+        }
+    }
+    else{
+        cout << a;
+    }
+
+}
