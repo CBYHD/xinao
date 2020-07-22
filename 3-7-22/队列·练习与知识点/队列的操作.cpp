0663f1d6c27cad12c6c32eda57afd17bd8962f6d
lenovo <lenovo@LAPTOP-EJ9ASDJ8>
2020-07-22 17:00:42 +0800

    GG20207221700

---
+++ 3-7-22/队列·练习与知识点/队列的操作.cpp
@@ -0,0 +1,23 @@
+#define N 101
+#include<iostream>
+using namespace std;
+int que[N+1],front,back;
+int main(){
+    int n;
+    cin >> n;
+    for(int i =0;i < n;i++){
+        que[back++] =i+1;
+
+    }
+    //获取队列中元素的个数
+    cout << back - front << endl;//排序通式
+    //获取队头元素的值
+    cout << que[front] << endl;
+    front++;
+    //获取队列中元素的个数
+    cout << back - front << endl;
+    return 0;
+}
+//         ----------------------------------------------
+//         |更多代码：github.com/cbyhd/xinao             |
+//         ----------------------------------------------
