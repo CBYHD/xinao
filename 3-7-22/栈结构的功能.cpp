fc7fb4021e68ad5b14b7b66c206d211d1feeb4c9
lenovo <lenovo@LAPTOP-EJ9ASDJ8>
2020-07-22 09:17:10 +0800

    GG

--- 3-7-22/栈结构的功能.cpp
+++ 3-7-22/栈结构的功能.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int stack[N],top;
 int main(){
 
+    /*
+    栈结构的功能
+    push() // 入栈
+    pop() // 出栈
+    top()//栈顶元素的位置
+    getTop //获取栈顶元素的值
+    size()//获取栈中元素的个数
+    empty // 判断栈是否为空
+    */
     return 0;
 }
-// 栈结构的功能
-// push() // 入栈
-// pop() // 出栈
-// top()//栈顶元素的位置
-// getTop //获取栈顶元素的值
-// size()//获取栈中元素的个数
-// empty // 判断栈是否为空
+//cbyhd 
